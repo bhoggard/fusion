c21e3ebd4b3079ed86003628ede862c79de93c83
Barry Hoggard <barry@artsymail.com>
2019-02-14 17:33:34 -0500

    Fixed creation of db session

--- src/main/scala/fusion/AnormRepo.scala
+++ src/main/scala/fusion/AnormRepo.scala
@@ -1,15 +1,36 @@
 package fusion
 
-import slick.jdbc.JdbcBackend.{ BaseSession, DatabaseDef }
+import java.sql.Connection
 
+import slick.jdbc.JdbcBackend.{ BaseSession, DatabaseDef }
 import anorm._
 import fusion.models.Author
 import slick.jdbc.JdbcBackend
 
 class AnormRepo(implicit db: JdbcBackend.Database) {
 
-  val connection                              = new UnmanagedSession(db)
+  def loadAuthor(id: Long): Option[Author] = withConnection { implicit c =>
+    SQL("SELECT * FROM authors WHERE id = {id}")
+      .on("id" -> id)
+      .as(authorParser.singleOpt)
+  }
+
   private val authorParser: RowParser[Author] = Macro.namedParser[Author]
+
+  private def getConnection(autocommit: Boolean = true): Connection = {
+    val connection = db.source.createConnection()
+    connection.setAutoCommit(autocommit)
+    connection
+  }
+
+  private def withConnection[A](block: Connection ⇒ A): A = {
+    val connection = getConnection()
+    try {
+      block(connection)
+    } finally {
+      connection.close()
+    }
+  }
 
 }
 
